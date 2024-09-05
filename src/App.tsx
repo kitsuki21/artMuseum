8fcbae9b13923a83fb42cab36dad9431717a0ee9
kitsuki <RavenLebron@yandex.by>
2024-09-05 21:27:15 +0300

    rack

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Home } from "src/pages/Home";
 import { Favorites } from "src/pages/Favorites";
 import { DetailDescription } from "src/pages/DetailDescription";
@@ -9,13 +9,13 @@ import { FavoriteProvader } from "./context/FavoritesProvader";
 function App() {
   return (
     <FavoriteProvader>
-      <BrowserRouter>
+      <HashRouter basename="/artMuseum">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/favorites" element={<Favorites />} />
           <Route path="/artwork/:id" element={<DetailDescription />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </FavoriteProvader>
   );
 }
